a6820fb22c7dbe5306c9a0e8ea196e22bb57fb0f
Vinod Nagabhushan Rao Shivaram Samanth <vinodnrssamanth@gmail.com>
2015-05-22 00:59:46 -0700

    Project Files added

---
+++ song.rb
@@ -0,0 +1,58 @@
+require 'dm-core'
+require 'dm-migrations'
+
+
+class StudentData
+  include DataMapper::Resource
+  property :id, Serial
+  property :sfname, String
+  property :slname, String
+  property :major, String
+  property :gpa, Float
+end
+
+configure do
+  enable :sessions
+  set :username, 'frank'
+  set :password, 'sinatra'
+end
+
+DataMapper.finalize
+
+get '/songs' do
+  @songs = StudentData.all
+  slim :songs
+end
+
+get '/songs/new' do
+  halt(401,'Not Authorized') unless session[:admin]
+  @song = StudentData.new
+  slim :new_song
+end
+
+get '/songs/:id' do
+  @song = StudentData.get(params[:id])
+  slim :show_song
+end
+
+get '/songs/:id/edit' do
+  @song = StudentData.get(params[:id])
+  slim :edit_song
+end
+
+post '/songs' do  
+  song = StudentData.create params[:song]
+  puts params[:song]
+  redirect to("/songs/#{song.id}")
+end
+
+put '/songs/:id' do
+  song = StudentData.get(params[:id])
+  song.update(params[:song])
+  redirect to("/songs/#{song.id}")
+end
+
+delete '/songs/:id' do
+  StudentData.get(params[:id]).destroy
+  redirect to('/songs')
+end
